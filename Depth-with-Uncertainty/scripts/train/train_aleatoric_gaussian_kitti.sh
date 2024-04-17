60b589db080d2cd4f37399b4c7919584d8387e08
Alessandro Lombardini <alessandro.lombardini@maggioli.it>
2024-04-17 16:23:15 +0200

    Changes to the scripts: new path

--- Depth-with-Uncertainty/scripts/train/train_aleatoric_gaussian_kitti.sh
+++ Depth-with-Uncertainty/scripts/train/train_aleatoric_gaussian_kitti.sh
@@ -1,4 +1,4 @@
-cd  /workspace/Supervised-Single-View-Depth-Learning-with-Uncertainty/src
+cd  /workspace/Depth-with-Uncertainty/src
 pip3 install --upgrade pip
 pip3 install -r requirements.txt
 python3 main.py --uncertainty "aleatoric.gaussian"
