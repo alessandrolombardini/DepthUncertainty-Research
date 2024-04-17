06fa4846c52fd223dd3d856855e748a7b3f174a6
Alessandro Lombardini <alessandro.lombardini@maggioli.it>
2024-04-17 16:34:57 +0200

    Change of get_dataloader

--- Depth-with-Uncertainty/src/data/__init__.py
+++ Depth-with-Uncertainty/src/data/__init__.py
@@ -7,8 +7,8 @@ import os
 
 
 def get_dataloader(config):
-    train_loader = DepthDataLoader(config, config.device, "train").data
-    test_loader = DepthDataLoader(config, config.device, "online_eval").data
+    train_loader = DepthDataLoader(config, "train", config.device).data
+    test_loader = DepthDataLoader(config, "online_eval", config.device).data
     
     data_loader = {'train': train_loader, 
                    'test': test_loader}
